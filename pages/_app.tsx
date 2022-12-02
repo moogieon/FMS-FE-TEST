882a819c7b8e39969b25b711e2a8e48b95d0e7b0
moogieon <vzlzydn5007@gmail.com>
2022-12-02 09:59:40 +0900

    전역 설정 및 로그인 기능 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useState,
+} from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+interface IContext {
+  accessToken?: string;
+  setAccessToken: Dispatch<SetStateAction<undefined>>;
+  userInfo: {
+    name: string;
+    id: string;
+  };
+  setUserInfo: Dispatch<SetStateAction<{ name: string; id: string }>>;
+}
+
+export const GlobalContext = createContext({} as IContext);
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [accessToken, setAccessToken] = useState();
+  const [userInfo, setUserInfo] = useState({ name: "", id: "" });
+  const value = {
+    accessToken,
+    setAccessToken,
+    userInfo,
+    setUserInfo,
+  };
+  return (
+    <GlobalContext.Provider value={value}>
+      <Component {...pageProps} />;
+    </GlobalContext.Provider>
+  );
 }
